82b5e227a8231ee362eafc44d04267019fe7e2c4
IkanJalan <nickyarvin@gmail.com>
2023-04-16 22:09:06 +0700

    Object

---
+++ penulisan_object.js
@@ -0,0 +1,41 @@
+// let personArr = ["Reza", "Kecap", "Male", 25];
+
+// console.log(personArr[0]);
+
+// let personObj = {
+//     firstName : "Reza",
+//     lastName : "Kecap",
+//     gender : "Male",
+//     age : 25
+// }
+
+// console.log(personObj.firstName);
+
+// let car = {}
+// // meng asign property (key): value object
+// car.merk = "Toyota",
+// car.color = "Silver",
+// car.type = "Sedan"
+
+// console.log(car);
+
+let game = {}
+game.name = "Minecraft",
+game.genre = "Sandbox",
+game.mode = "Single/Multiplayer Player"
+
+console.log(game);
+
+let keretaApi = {}
+keretaApi.bahanBakar = "Batu Bara",
+keretaApi.gerbong = "Penumpang/Kargo",
+keretaApi.warna = "Putih"
+
+console.log(keretaApi);
+
+let motor = {}
+motor.merk = "Yamaha"
+motor.warna = "Merah"
+motor.jenis = "Bensin/Listrik"
+
+console.log(motor);
